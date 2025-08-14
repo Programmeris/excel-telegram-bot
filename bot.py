63e303a17cdd1b8b46a588b272f38b8c97857b72
fibbanachi <49879947+Programmeris@users.noreply.github.com>
2025-08-14 12:26:50 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,16 +10,27 @@ async def find(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     if update.message and update.message.text:
         workbook = load_workbook(XLSX_FILE_PATH)
         sheet = workbook.active
+        headers = get_headers(sheet)
+        keywords_column = headers.get('Keywords')
+        links_column = headers.get('Link')
         response_message = ''
 
-        for row in sheet.iter_rows():
-            parsed_message = update.message.text.replace("/find", "").strip()
-            keywords = str(row[0].value).split(";")
-            keyword_match = any(element in parsed_message for element in keywords)
-            if keyword_match:
-                response_message += row[1].value
+        if keywords_column and links_column:
+            for row in sheet.iter_rows():
+                parsed_message = update.message.text.replace("/find", "").strip()
+                keywords = str(row[keywords_column - 1].value).replace(" ", "").split(";")
+                keyword_match = any(element in parsed_message for element in keywords)
+                if keyword_match:
+                    response_message += row[links_column - 1].value
                 
         await update.message.reply_text(f"{response_message}")
+
+def get_headers(sheet: any ) -> dict:
+    headers = {}
+    for col_idx, cell in enumerate(sheet[1], start=1):
+        headers[cell.value] = col_idx
+    
+    return headers
 
 def main():
     try:
